aaad9e7f291b9bf330fdc81f6328428063a2281e
999tasneemahmed@gmail.com <999tasneemahmed@gmail.com>
2021-11-02 08:07:56 +0000

    2nd transforming: replace values of job_post_rating <0 into 0

--- models/staging/stg_uncleaned_ds_jobs_glassdor.sql
+++ models/staging/stg_uncleaned_ds_jobs_glassdor.sql
@@ -3,5 +3,6 @@ select
     job_title,
     cast( {{ var("salary_range") }}[ORDINAL(1)] as int64) * 1000  AS min_salary_estimate,
     cast( {{ var("salary_range") }}[ORDINAL(2)] as int64) * 1000  AS max_salary_estimate,
+    IF( job_post_rating<0, 0.0, job_post_rating) as job_post_rating
 from 
   `ds-jobs-analysis.DS_Jops_Glassdor.raw_uncleaned_ds_jobs_glassdor`
