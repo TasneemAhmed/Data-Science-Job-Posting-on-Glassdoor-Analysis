d36c36a08124ade2566adb579202551a6af3a6f1
999tasneemahmed@gmail.com <999tasneemahmed@gmail.com>
2021-11-02 08:26:24 +0000

    3rd transforming: remove rating(numbers) from company_name

--- models/staging/stg_uncleaned_ds_jobs_glassdor.sql
+++ models/staging/stg_uncleaned_ds_jobs_glassdor.sql
@@ -3,6 +3,8 @@ select
     job_title,
     cast( {{ var("salary_range") }}[ORDINAL(1)] as int64) * 1000  AS min_salary_estimate,
     cast( {{ var("salary_range") }}[ORDINAL(2)] as int64) * 1000  AS max_salary_estimate,
-    IF( job_post_rating<0, 0.0, job_post_rating) as job_post_rating
+    IF( job_post_rating<0, 0.0, job_post_rating) as job_post_rating,
+    REGEXP_REPLACE(company_name, '[^a-zA-z ]', '') as company_name,
+    company_location,
 from 
   `ds-jobs-analysis.DS_Jops_Glassdor.raw_uncleaned_ds_jobs_glassdor`
