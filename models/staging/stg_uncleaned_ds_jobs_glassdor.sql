e52cd595dd32f7a68c522e2734b55821a6a463e7
999tasneemahmed@gmail.com <999tasneemahmed@gmail.com>
2021-11-02 11:14:53 +0000

    5th transforming: replace company_size = '-1'  with 'Unknown'

--- models/staging/stg_uncleaned_ds_jobs_glassdor.sql
+++ models/staging/stg_uncleaned_ds_jobs_glassdor.sql
@@ -7,5 +7,6 @@ select
     REGEXP_REPLACE(company_name, '[^a-zA-z ]', '') as company_name,
     company_location,
     IF( CONTAINS_SUBSTR(headquarters_location, '-1'), company_location, headquarters_location) as headquarters_location,
+    IF( CONTAINS_SUBSTR(company_size, '-1'), 'Unknown', company_size) as company_size,
 from 
   `ds-jobs-analysis.DS_Jops_Glassdor.raw_uncleaned_ds_jobs_glassdor`
